534c9147a258316b3c58aca9c261f5dea95e71e9
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-02-02 22:14:57 +0500

    Deploy

--- lib/website-deployement-stack.ts
+++ lib/website-deployement-stack.ts
@@ -1,9 +1,35 @@
 import * as cdk from '@aws-cdk/core';
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+
+
 
 export class WebsiteDeployementStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+    });
+
+    const display = new cdk.CfnOutput (this, 'DistributionDomainName', {
+      value: distribution.domainName
+    });
+    
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./myWeb')],
+      destinationBucket: websiteBucket,
+      distribution:distribution
+    });
+
   }
 }
